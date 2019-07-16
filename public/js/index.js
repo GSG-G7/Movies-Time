208865af2b9c491c08e35a663ca94cfa54e33917
MaiUbeid <ubead95@gmail.com>
2019-07-16 14:59:03 +0300

    add files changed for coding movie request Relates #6

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+const moviesRequest = callback => {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let response = JSON.parse(xhr.responseText);
+      let movies = response.results;
+      callback(movies);
+    }
+  };
+  xhr.open(
+    "GET",
+    "https://api.themoviedb.org/3/movie/popular?api_key=fe9a692601a10b1c8f689d99796cbb82&language=en-US&page=1"
+  );
+  xhr.send();
+};
+
+if (typeof module !== "undefined") {
+  module.exports = moviesRequest;
+}
---
+++ public/test/test.js
@@ -0,0 +1,2 @@
+const index = require("../js/index");
+const tape = require("tape");
