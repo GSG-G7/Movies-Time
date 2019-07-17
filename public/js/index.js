06566c0fb2a02675f2a50b7a3b9cb1e49d6935ae
asem1789 <as.1789@hotmail.com>
2019-07-17 11:13:33 +0300

    make some refactor for dom and index file Relates #13

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,37 @@
-const urlMovies = `https://api.themoviedb.org/3/movie/popular?api_key=fe9a692601a10b1c8f689d99796cbb82&language=en-US&page=1`;
 
-const urlNews = "https://newsapi.org/v2/everything?";
-const queryNews = "q=movie&from=2019-06-16&sortBy=publishedAt";
-const newsAPI_key = "&apiKey=c97ecb4a147a402899bf700fb3acf8e5";
+function request(url, callback) {
 
-function getNews(query, callback) {
-  const newsUrl = urlNews + query + newsAPI_key;
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = _ => {
     if (xhr.readyState === 4 && xhr.status === 200) {
       const obj = JSON.parse(xhr.responseText);
-      // console.log(obj.articles);
-      callback(obj.articles);
+      callback(obj);
     }
   };
-  xhr.open("GET", newsUrl);
+  xhr.open("GET", url);
   xhr.send();
 }
 
-const moviesRequest = callback => {
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      let response = JSON.parse(xhr.responseText);
-      let movies = response.results;
-      callback(movies);
-    }
-  };
-  xhr.open("GET", urlMovies);
-  xhr.send();
-};
+function getMovies(callback){
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=fe9a692601a10b1c8f689d99796cbb82&language=en-US&page=1`;
+
+  request(url , (obj)=>{
+    callback(obj.results);
+  })
+}
+
+function getNews(callback){
+  const url = "https://newsapi.org/v2/everything?q=movie&apiKey=c97ecb4a147a402899bf700fb3acf8e5";
+  request(url , (obj)=>{
+    callback(obj.articles);
+  })
+
+}
+
+
+
+
+
 
 if (typeof module !== "undefined") {
   module.exports = { moviesRequest, pages };
