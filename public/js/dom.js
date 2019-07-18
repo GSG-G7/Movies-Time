47a39c9ba87a03fe0d1dcda133ddd5097729c1c6
Asmaathabet <asmathabet200@hotmail.com>
2019-07-18 10:39:58 +0300

    solve some issues

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,105 +4,118 @@ const prevBtn = document.querySelector(".prev");
 const newsContainer = document.getElementById("news");
 
 getMovies(arr => {
-  const movieArr = [];
-  arr.some((element, index) => {
-    movieArr.push({
-      title: element.title,
-      poster: element.poster_path,
-      lang: element.original_language,
-      overview: element.overview,
-      releaseDate: element.release_date
-    });
-    return index === 11;
-  });
+    const movieArr = [];
+    arr.some((element, index) => {
+        movieArr.push({
+            title: element.title,
+            poster: element.poster_path,
+            lang: element.original_language,
+            overview: element.overview,
+            releaseDate: element.release_date,
+            movieId: element.id
+        });
+        return index === 11;
 
-  manageRender("movie", movieArr);
+    });
+
+    manageRender("movie", movieArr);
 });
 
 function manageRender(check, arr) {
-  if (check === "movie") {
-    arr.forEach(element => {
-      createMovie(element);
-    });
-  } else if (check === "news") {
-    arr.forEach(element => {
-      showNews(element);
-    });
-  }
+    if (check === "movie") {
+        arr.forEach(element => {
+            createMovie(element);
+        });
+    } else if (check === "news") {
+        arr.forEach(element => {
+            showNews(element);
+        });
+    }
 }
 
 getNews(arr => {
-  const newsArr = [];
-  // This Method for get specific number of element from original array
-  arr.some((element, index) => {
-    newsArr.push({
-      imageLink: element.urlToImage,
-      title: element.title,
-      description: element.description,
-      source: element.url,
-      author: element.author,
-      published: element.publishedAt
+    const newsArr = [];
+    // This Method for get specific number of element from original array
+    arr.some((element, index) => {
+        newsArr.push({
+            imageLink: element.urlToImage,
+            title: element.title,
+            description: element.description,
+            source: element.url,
+            author: element.author,
+            published: element.publishedAt
+        });
+
+        return index === 5;
     });
 
-    return index === 5;
-  });
-
-  manageRender("news", newsArr);
+    manageRender("news", newsArr);
 });
+/* getTrialers(arr,key) => {
+    
+  
+}); */
 
 const createMovie = obj => {
-  let movieDiv = document.createElement("div");
-  let infoDiv = document.createElement("div");
-  movieDiv.classList.add("movie-card");
-  infoDiv.classList.add("movie-info");
-  let movieTitle = element("h2", obj.title);
-  let moviePoster = document.createElement("img");
-  moviePoster.setAttribute("alt", "moviesImages");
-  let movieLang = element("span", obj.lang);
-  let moviePar = element("p", obj.overview);
-  let movieDate = element("span", obj.releaseDate);
-  moviePoster.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${
-    obj.poster
-  }`;
-  infoDiv.appendChild(movieTitle);
-  movieDiv.appendChild(moviePoster);
-  infoDiv.appendChild(movieLang);
-  infoDiv.appendChild(moviePar);
-  infoDiv.appendChild(movieDate);
-  movieDiv.appendChild(infoDiv);
-  moviesSection.appendChild(movieDiv);
+    const movieDiv = document.createElement("div");
+    const infoDiv = document.createElement("div");
+    movieDiv.classList.add("movie-card");
+    infoDiv.classList.add("movie-info");
+    const movieTitle = element("h2", obj.title);
+    const moviePoster = document.createElement("img");
+    moviePoster.setAttribute("alt", "moviesImages");
+    const movieLang = element("span", obj.lang);
+    const langheading = element("p", "Language :  ");
+    langheading.appendChild(movieLang);
+    const moviePar = element("p", obj.overview);
+    const overviewheading = element("p", "Overview :  ");
+    overviewheading.appendChild(moviePar);
+    const movieDate = element("span", obj.releaseDate);
+    const releaseDateheading = element("p", "Release Date:  ");
+    releaseDateheading.appendChild(movieDate);
+
+    moviePoster.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${obj.poster}`;
+    infoDiv.appendChild(movieTitle);
+    movieDiv.appendChild(moviePoster);
+    infoDiv.appendChild(langheading);
+    infoDiv.appendChild(overviewheading);
+    infoDiv.appendChild(releaseDateheading);
+    movieDiv.appendChild(infoDiv);
+    moviesSection.appendChild(movieDiv);
 };
+
+
 
 // Create element in browser
 const element = (e, content) => {
-  let x = document.createElement(e);
-  x.textContent = content;
-  x.className = "movie-content";
-  return x;
+    let x = document.createElement(e);
+    x.textContent = content;
+    x.className = "movie-content";
+    return x;
 };
 
 function showNews(obj) {
-  const newsbox = document.createElement("div");
-  newsbox.classList.add("news-card");
-  const newsImag = document.createElement("img");
-  const title = document.createElement("h2");
-  const desc = document.createElement("p");
-  const source = document.createElement("a");
+    const newsbox = document.createElement("div");
+    newsbox.classList.add("news-card");
+    const newsImag = document.createElement("img");
+    const title = document.createElement("h2");
+    const desc = document.createElement("p");
+    const source = document.createElement("a");
 
-  // Set attributes
-  newsImag.setAttribute("src", obj.imageLink);
-  newsImag.setAttribute("alt", "NewsImage");
-  title.textContent = obj.title;
-  desc.textContent = obj.description;
-  source.textContent = "Read More";
-  source.setAttribute("href", obj.source);
-  source.setAttribute("target", "_blank");
+    // Set attributes
+    newsImag.setAttribute("src", obj.imageLink);
+    newsImag.setAttribute("alt", "NewsImage");
+    title.textContent = obj.title;
+    desc.textContent = obj.description;
+    source.textContent = "Read More";
+    source.setAttribute("href", obj.source);
+    source.setAttribute("target", "_blank");
 
-  // Append
-  newsbox.appendChild(newsImag);
-  newsbox.appendChild(title);
-  newsbox.appendChild(desc);
-  newsbox.appendChild(source);
+    // Append
+    newsbox.appendChild(newsImag);
+    newsbox.appendChild(title);
+    newsbox.appendChild(desc);
+    newsbox.appendChild(source);
 
-  newsContainer.appendChild(newsbox);
+    newsContainer.appendChild(newsbox);
 }
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-
 const urlMovies = `https://api.themoviedb.org/3/movie/popular?api_key=fe9a692601a10b1c8f689d99796cbb82&language=en-US&page=1`;
-
 const urlNews = "https://newsapi.org/v2/everything?";
 const queryNews = "q=movie";
 const newsAPI_key = "&apiKey=c97ecb4a147a402899bf700fb3acf8e5";
@@ -33,31 +31,41 @@ const moviesRequest = callback => {
 };
 
 function request(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = _ => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const obj = JSON.parse(xhr.responseText);
-      callback(obj);
-    }
-  };
-  xhr.open("GET", url);
-  xhr.send();
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = _ => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const obj = JSON.parse(xhr.responseText);
+            callback(obj);
+        }
+    };
+    xhr.open("GET", url);
+    xhr.send();
 }
 
 function getMovies(callback) {
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=fe9a692601a10b1c8f689d99796cbb82&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=fe9a692601a10b1c8f689d99796cbb82&language=en-US&page=1`;
 
-  request(url, obj => {
-    callback(obj.results);
-  });
+    request(url, obj => {
+        callback(obj.results);
+    });
 }
 
 function getNews(callback) {
-  const url =
-    "https://newsapi.org/v2/everything?q=movie&apiKey=c97ecb4a147a402899bf700fb3acf8e5";
-  request(url, obj => {
-    callback(obj.articles);
-  });
+    const url =
+        "https://newsapi.org/v2/everything?q=movie&apiKey=c97ecb4a147a402899bf700fb3acf8e5";
+    request(url, obj => {
+        callback(obj.articles);
+    });
+}
+
+function getTrialers(callback) {
+    const url =
+        `http://api.themoviedb.org/3/movie/429617/videos?api_key=fe9a692601a10b1c8f689d99796cbb82`;
+    request(url, obj => {
+        console.log(obj);
+        callback(obj);
+
+    });
 }
 
 if (typeof module !== "undefined") {
